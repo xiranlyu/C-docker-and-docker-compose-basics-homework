eb1967517d045efcc286a63925f50852cee95a64
Xiran LYU <xiran.lyu@thoughtworks.com>
2020-10-27 16:00:21 +0800

    homework done, can see 'Hello from Bob!' from browser

--- alice/src/main/java/com/thoughtworks/capability/gtb/alice/AliceController.java
+++ alice/src/main/java/com/thoughtworks/capability/gtb/alice/AliceController.java
@@ -2,12 +2,15 @@ package com.thoughtworks.capability.gtb.alice;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class AliceController {
 
   @GetMapping("/hello")
   public String hello() {
-    return "";
+    RestTemplate restTemplate = new RestTemplate();
+    String getUrl = "http://bob:8081/hello";
+    return restTemplate.getForObject(getUrl, String.class);
   }
 }
